510fb9fbf7b98e8fb204515853cca6e2f0b4f625
ahir vipul <114806840+vipulahir7@users.noreply.github.com>
2025-05-30 22:15:22 +0530

    Fixing test api

--- Backend/server.js
+++ Backend/server.js
@@ -103,6 +103,7 @@ app.get("/", (req, res) => {
 // });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
 });
